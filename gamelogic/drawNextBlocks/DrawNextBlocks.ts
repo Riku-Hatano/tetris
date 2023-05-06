11c3de190d2eb7d30611b0665e9405f6efecf2cc
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-05-06 14:37:16 -0700

    implemented next blocks

---
+++ gamelogic/drawNextBlocks/DrawNextBlocks.ts
@@ -0,0 +1,103 @@
+import next from "next/types";
+import Game from "../../pages/game";
+import { nextBlocks } from "../Main";
+import GameStatus from "../status";
+
+const DrawNextBlocks = () => {
+    let x = 20;
+    let y = 20;
+
+    if(nextBlocks !== undefined) {
+        nextBlocks.clearRect(10, 10, 50, 100);
+        nextBlocks.fillStyle = "black";
+        nextBlocks.fillRect(10, 10, 50, 100);
+
+        for(let i = 0 ; i < 3 ; i++) {
+            switch(GameStatus.nextBlock[i]) {
+                case "i":
+                    nextBlocks.fillStyle = "#00ffff";
+                    nextBlocks.fillRect(x - 5, y, 40, 10);
+                    break;
+                case "o":
+                    nextBlocks.fillStyle = "#ffff00";
+                    nextBlocks.fillRect(x + 5, y, 20, 20);
+                    break;
+                case "t":
+                    nextBlocks.beginPath();
+                    nextBlocks.moveTo(x + 10, y + 10);
+                    nextBlocks.lineTo(x + 10, y);
+                    nextBlocks.lineTo(x + 20, y);
+                    nextBlocks.lineTo(x + 20, y + 10);
+                    nextBlocks.lineTo(x + 30, y + 10);
+                    nextBlocks.lineTo(x + 30, y + 20);
+                    nextBlocks.lineTo(x, y + 20);
+                    nextBlocks.lineTo(x, y + 10);
+                    nextBlocks.lineTo(x + 10, y + 10);
+                    nextBlocks.closePath();
+                    nextBlocks.fillStyle = "#800080";
+                    nextBlocks.fill();
+                    break;
+                case "z":
+                    nextBlocks.beginPath();
+                    nextBlocks.moveTo(x + 10, y + 10);
+                    nextBlocks.lineTo(x, y + 10);
+                    nextBlocks.lineTo(x, y);
+                    nextBlocks.lineTo(x + 20, y);
+                    nextBlocks.lineTo(x + 20, y + 10);
+                    nextBlocks.lineTo(x + 30, y + 10);
+                    nextBlocks.lineTo(x + 30, y + 20);
+                    nextBlocks.lineTo(x + 10, y + 20);
+                    nextBlocks.lineTo(x + 10, y + 10);
+                    nextBlocks.closePath();
+                    nextBlocks.fillStyle = "#ff0000";
+                    nextBlocks.fill();
+                    break;
+                case "s":
+                    nextBlocks.beginPath();
+                    nextBlocks.moveTo(x + 10, y + 10);
+                    nextBlocks.lineTo(x + 10, y);
+                    nextBlocks.lineTo(x + 30, y);
+                    nextBlocks.lineTo(x + 30, y + 10);
+                    nextBlocks.lineTo(x + 20, y + 10);
+                    nextBlocks.lineTo(x + 20, y + 20);
+                    nextBlocks.lineTo(x, y + 20);
+                    nextBlocks.lineTo(x, y + 10);
+                    nextBlocks.lineTo(x + 10, y + 10);
+                    nextBlocks.closePath();
+                    nextBlocks.fillStyle = "#00ff00";
+                    nextBlocks.fill();
+                    break;
+                case "j":
+                    nextBlocks.beginPath();
+                    nextBlocks.moveTo(x + 10, y + 10);
+                    nextBlocks.lineTo(x + 30, y + 10);
+                    nextBlocks.lineTo(x + 30, y + 20);
+                    nextBlocks.lineTo(x, y + 20);
+                    nextBlocks.lineTo(x, y);
+                    nextBlocks.lineTo(x + 10, y);
+                    nextBlocks.lineTo(x + 10, y + 10);
+                    nextBlocks.closePath();
+                    nextBlocks.fillStyle = "#0000ff";
+                    nextBlocks.fill();
+                    break;
+                case "l":
+                    nextBlocks.beginPath();
+                    nextBlocks.moveTo(x + 10, y + 10);
+                    nextBlocks.lineTo(x + 20, y + 10);
+                    nextBlocks.lineTo(x + 20, y);
+                    nextBlocks.lineTo(x + 30, y);
+                    nextBlocks.lineTo(x + 30, y + 20);
+                    nextBlocks.lineTo(x, y + 20);
+                    nextBlocks.lineTo(x, y + 10);
+                    nextBlocks.lineTo(x + 10, y + 10);
+                    nextBlocks.closePath();
+                    nextBlocks.fillStyle = "#ff7f00";
+                    nextBlocks.fill();
+                    break;
+            }
+            y += 30;
+        }
+    }
+}
+
+export default DrawNextBlocks;
