667bdb5c74d975ab56c051ca6cfe718e18d0085d
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-05-10 14:33:25 -0700

    implemented nav depending on login situation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,53 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
-import CheckLogUser from "../utils/CheckLogUser";
+// import CheckLogUser from "../utils/CheckLogUser";
 
 const Header = () => {
-    const logUser = CheckLogUser();
+    const [path, setPath] = useState("");
+    const router = useRouter();
+    const [logUser, setLogUser] = useState(null);
+
+    useEffect(() => { //This useEffect will run in the first rendering, which means just after web page is refreshed.
+        if(sessionStorage.getItem("logUser") !== null) {
+            // console.log(sessionStorage.getItem("logUser"));
+            setLogUser(JSON.parse(sessionStorage.getItem("logUser"))[0].name);
+            console.log(logUser);
+        } else {
+            console.log("no logged in user");
+        }
+    }, []);
+    useEffect(() => { //This useEffect will run after second rendering. Only with the useEffecth which doesn't contain the pathname as the watching variable.
+        if(sessionStorage.getItem("logUser") !== null) {
+            // console.log(sessionStorage.getItem("logUser"));
+            setLogUser(JSON.parse(sessionStorage.getItem("logUser"))[0].name);
+            console.log(logUser);
+        } else {
+            console.log("no logged in user");
+        }
+    }, [router.pathname]);
+    // const [path, setPath] = useState("");
+    // const router = useRouter();
+    // let logUser = null;
+
+    // useEffect(() => { //This useEffect will run in the first rendering, which means just after web page is refreshed.
+    //     if(sessionStorage.getItem("logUser") !== null) {
+    //         // console.log(sessionStorage.getItem("logUser"));
+    //         logUser = JSON.parse(sessionStorage.getItem("logUser"))[0].name;
+    //         console.log(logUser);
+    //     } else {
+    //         console.log("no logged in user");
+    //     }
+    // }, []);
+    // useEffect(() => { //This useEffect will run after second rendering. Only with the useEffecth which doesn't contain the pathname as the watching variable.
+    //     if(sessionStorage.getItem("logUser") !== null) {
+    //         // console.log(sessionStorage.getItem("logUser"));
+    //         logUser = JSON.parse(sessionStorage.getItem("logUser"))[0].name;
+    //         console.log(logUser);
+    //     } else {
+    //         console.log("no logged in user");
+    //     }
+    // }, [router.pathname]);
     return (
         <>
             <ul>
@@ -11,6 +56,7 @@ const Header = () => {
                 { logUser ? null : <li><Link href="/login">login</Link></li> }
                 { logUser ? null : <li><Link href="/register">register</Link></li> }
                 { logUser ? <li><Link href="/user">user</Link></li> : null }
+                { logUser ? null : <li><Link href="/user">user for null</Link></li>}
             </ul>
         </>
     )
