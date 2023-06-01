2578f9237424252f882323639ab7f3e0429da6f7
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-05-31 18:04:18 -0700

    ttt

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -32,7 +32,7 @@ const Login = () => {
         //         console.log(rej);
         //     }
         // )
-        axios.create().post(`${axiosconfig.baseURL}pages/api/lib/pseudoServices/pseudoLogin`, inputVal).then(
+        axios.create().post("../api/lib/pseudoServices/pseudoLogin.js", inputVal).then(
             (res) => {
                 if(res.data.message != null) {
                     router.push("/user");
