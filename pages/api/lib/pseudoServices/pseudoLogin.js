97fca5997547ad8f1cb577872e400fae11a37b4b
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-05-31 18:55:13 -0700

    test

--- pages/api/lib/pseudoServices/pseudoLogin.js
+++ pages/api/lib/pseudoServices/pseudoLogin.js
@@ -6,22 +6,22 @@ const pseudoLogin = (req, res) => {
             res.status(200).json({message: "pseudoLogin: GET"});
             break;
         case "POST":
-            
+            res.status(200).json({message: "pseudoLogin: POST"});
             // fs.readFile("pages/api/pseudoDB/users.json", "utf-8", (err, data) => {
-            fs.readFile("https://tetris-riku-hatano.vercel.app/pages/api/pseudoDB/users.json", "utf-8", (err, data) => {
-                if(err) {
-                    console.log(err);
-                    res.status(400).json({message: "failed to read file"});
-                } else {
-                    JSON.parse(data).forEach((user) => {
-                        if(user.pw === req.body.pw && user.name === req.body.name) {
-                            res.status(200).json({message: [user]});
-                            return;
-                        }
-                    })
-                    res.status(200).json({message: null});
-                }
-            })
+            // fs.readFile("https://tetris-riku-hatano.vercel.app/pages/api/pseudoDB/users.json", "utf-8", (err, data) => {
+            //     if(err) {
+            //         console.log(err);
+            //         res.status(400).json({message: "failed to read file"});
+            //     } else {
+            //         JSON.parse(data).forEach((user) => {
+            //             if(user.pw === req.body.pw && user.name === req.body.name) {
+            //                 res.status(200).json({message: [user]});
+            //                 return;
+            //             }
+            //         })
+            //         res.status(200).json({message: null});
+            //     }
+            // })
             break;
     }
 }
