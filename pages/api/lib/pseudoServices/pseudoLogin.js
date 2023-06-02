896948cfe44f8c02778ffebdeab76a35543dceb4
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-06-02 15:05:43 -0700

    try post method

--- pages/api/lib/pseudoServices/pseudoLogin.js
+++ pages/api/lib/pseudoServices/pseudoLogin.js
@@ -3,47 +3,47 @@ const fs = require("fs");
 const pseudoLogin = (req, res) => {
     switch(req.method) {
         case "GET":
-            // res.status(200).json({message: "pseudoLogin: GET"});
-            fs.readFile("/var/task/api/pseudoDB/users.json", "utf-8", (err, data) => {
-                if(err) {
-                    console.log(err);
-                    res.status(400).json({
-                        message: req.body,
-                        cwd: process.cwd(),
-                        parentDir: path.resolve(__dirname, ".."),
-                    });
-                } else {
-                    JSON.parse(data).forEach((user) => {
-                        if(user.pw === req.body.pw && user.name === req.body.name) {
-                            res.status(200).json({message: [user]});
-                            return;
-                        }
-                    })
-                    res.status(200).json({message: null});
-                }
-            })
+            res.status(200).json({message: "pseudoLogin: GET"});
+            // fs.readFile("/var/task/api/pseudoDB/users.json", "utf-8", (err, data) => {
+            //     if(err) {
+            //         console.log(err);
+            //         res.status(400).json({
+            //             message: req.body,
+            //             cwd: process.cwd(),
+            //             parentDir: path.resolve(__dirname, ".."),
+            //         });
+            //     } else {
+            //         JSON.parse(data).forEach((user) => {
+            //             if(user.pw === req.body.pw && user.name === req.body.name) {
+            //                 res.status(200).json({message: [user]});
+            //                 return;
+            //             }
+            //         })
+            //         res.status(200).json({message: null});
+            //     }
+            // })
             break;
         case "POST":
-            // res.status(200).json({message: "pseudoLogin: POST"});//ハンドシェイク自体はうまくいってるっぽい
+            res.status(200).json({message: "pseudoLogin: POST"});//ハンドシェイク自体はうまくいってるっぽい
             // fs.readFile("pages/api/pseudoDB/users.json", "utf-8", (err, data) => {
-            fs.readFile("/var/task/api/pseudoDB/users.json", "utf-8", (err, data) => {
-                if(err) {
-                    console.log(err);
-                    res.status(400).json({
-                        message: req.body,
-                        cwd: process.cwd(),
-                        parentDir: path.resolve(__dirname, ".."),
-                    });
-                } else {
-                    JSON.parse(data).forEach((user) => {
-                        if(user.pw === req.body.pw && user.name === req.body.name) {
-                            res.status(200).json({message: [user]});
-                            return;
-                        }
-                    })
-                    res.status(200).json({message: null});
-                }
-            })
+            // fs.readFile("/var/task/api/pseudoDB/users.json", "utf-8", (err, data) => {
+            //     if(err) {
+            //         console.log(err);
+            //         res.status(400).json({
+            //             message: req.body,
+            //             cwd: process.cwd(),
+            //             parentDir: path.resolve(__dirname, ".."),
+            //         });
+            //     } else {
+            //         JSON.parse(data).forEach((user) => {
+            //             if(user.pw === req.body.pw && user.name === req.body.name) {
+            //                 res.status(200).json({message: [user]});
+            //                 return;
+            //             }
+            //         })
+            //         res.status(200).json({message: null});
+            //     }
+            // })
             break;
     }
 }
