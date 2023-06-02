6c95539ac4c1643e98431c056637d0ebaab06949
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-06-02 15:23:44 -0700

    try

--- pages/api/lib/pseudoServices/pseudoLogin.js
+++ pages/api/lib/pseudoServices/pseudoLogin.js
@@ -33,24 +33,38 @@ const pseudoLogin = (req, res) => {
             });//ハンドシェイク自体はうまくいってるっぽい
 
             // fs.readFile("pages/api/pseudoDB/users.json", "utf-8", (err, data) => {
-            // fs.readFile("/var/task/api/pseudoDB/users.json", "utf-8", (err, data) => {
-            //     if(err) {
-            //         console.log(err);
-            //         res.status(400).json({
-            //             message: req.body,
-            //             cwd: process.cwd(),
-            //             parentDir: path.resolve(__dirname, ".."),
-            //         });
-            //     } else {
-            //         JSON.parse(data).forEach((user) => {
-            //             if(user.pw === req.body.pw && user.name === req.body.name) {
-            //                 res.status(200).json({message: [user]});
-            //                 return;
-            //             }
-            //         })
-            //         res.status(200).json({message: null});
-            //     }
-            // })
+            fs.readFile("/var/task/.next/server/pages/api/pseudoDB/users.json", "utf-8", (err, data) => {
+                if(err) {
+                    console.log(err);
+                    res.status(400).json({
+                        message: req.body,
+                        cwd: process.cwd(),
+                        parentDir: path.resolve(__dirname, ".."),
+                    });
+                } else {
+                    JSON.parse(data).forEach((user) => {
+                        if(user.pw === req.body.pw && user.name === req.body.name) {
+                            res.status(200).json({message: [user]});
+                            return;
+                        }
+                    })
+                    res.status(200).json({message: null});
+                }
+            })
+
+
+            // cwd
+            // : 
+            // "/var/task"
+            // message
+            // : 
+            // "pseudoLogin: POST"
+            // parentDir
+            // : 
+            // "/var/task/.next/server/pages/api/lib"
+            // reqBody
+            // : 
+            // {name: 'test', pw: 'test'}
             break;
     }
 }
