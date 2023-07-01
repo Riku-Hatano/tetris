ba3271df8a4660cf9d3db6265b22882df4c2fe51
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-07-01 11:44:53 -0700

    changed axios config for production environment

--- pages/api/lib/axios/axiosconfig.js
+++ pages/api/lib/axios/axiosconfig.js
@@ -1,4 +1,4 @@
 export const axiosconfig = {
-    // baseURL: "https://tetris-riku-hatano.vercel.app/"
-    baseURL: "http://localhost:3000/"    
+    baseURL: "https://tetris-riku-hatano.vercel.app/"
+    // baseURL: "http://localhost:3000/"    
 }
