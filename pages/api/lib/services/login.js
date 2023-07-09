804f4df26ceb99040a3d18e715fdddafb56dcab0
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-07-08 23:30:01 -0700

    fixed illegal login and chenge the style of table in user page

--- pages/api/lib/services/login.js
+++ pages/api/lib/services/login.js
@@ -1,7 +1,6 @@
 import { createClient } from "contentful";
-import { getUsers } from "../contentful/getUsers";
 
-const login = async (req, res) => {
+const login = (req, res) => {
     // switch(req.method) { //this is for database.
     //     case "GET":
     //         res.status(200).json({ message: "connected as get request" });
@@ -25,14 +24,46 @@ const login = async (req, res) => {
             res.status(200).json({ message: "get method" });
             break;
         case "POST":
-            try {
-                const items = await getUsers();
-                console.log(items);
-                res.status(200).json({ message: null });
-              } catch (error) {
-                console.log(error);
-                res.status(400).json({ message: "failed to get data" });
-              }
+            const client = createClient({
+                space: "jivp4q6rn93f",
+                accessToken: "lX7fWPWoJdKgbnEgSCU2kSEGlEBT0H1PFqdWiuntS3s"
+            })
+            let entry;
+            client.getEntries({ content_type: "tetrisusers" }).then(
+                (resp) => {
+                    entry = {
+                        fields: {
+                            uname: {
+                                "en-US": req.body.uname
+                            },
+                            pw: {
+                                "en-US": req.body.pw
+                            },
+                            regdate: {
+                                "en-US": req.body.regdate
+                            },
+                            uid: {
+                                "en-US": resp.total
+                            }
+                        }
+                    }
+                    let returnObj = null;
+                    resp.items.forEach((user) => {
+                        if(user.fields.uname === req.body.uname && user.fields.pw === req.body.pw) {
+                            returnObj = user.fields;
+                        }
+                    })
+                    if(returnObj === null) {
+                        res.status(200).json({ message: null });
+                    } else {
+                        res.status(200).json({ message: returnObj });
+                    }
+                },
+                (rejc) => {
+                    res.status(400).json({ message: "failed to get data" });
+                    console.log(rejc);
+                }
+            )
             break;
     }
 }
