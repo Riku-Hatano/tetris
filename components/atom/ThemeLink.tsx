ef16f996d631049734a4d89c58ab9c9bd9f4269f
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2024-03-03 23:04:10 +0900

    fix: fixed header component  and started to use Atomic design

---
+++ components/atom/ThemeLink.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styled from "@emotion/styled";
+import theme from "../../theme/theme";
+
+const ThemeLink = styled(Link)(() => ({
+  width: "100%",
+  height: "100%",
+}));
+
+export default ThemeLink;
